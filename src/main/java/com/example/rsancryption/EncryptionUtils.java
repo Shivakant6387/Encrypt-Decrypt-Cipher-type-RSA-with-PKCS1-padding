532ce37aa53860b0ad6bfcc6627f312dee91ad9e
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-13 16:41:49 +0530

    access token and encrypted msg

--- src/main/java/com/example/rsancryption/EncryptionUtils.java
+++ src/main/java/com/example/rsancryption/EncryptionUtils.java
@@ -1,22 +1,66 @@
 package com.example.rsancryption;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.security.*;
 import javax.crypto.*;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.Map;
+import java.util.Objects;
 
+@Service
 public class EncryptionUtils {
-    private static final String PUBLIC_KEY_STRING = "-----BEGIN PUBLIC KEY-----\r\n" +
-            "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCTCpq86HNKMcWrrf4kJ/l1YBRd\r\n" +
-            "yin4mVDrybj2xQ1APDM2ONJbEz6j6/mv/qcbFeJsq5fHOU7nXD6lF64yo2FWbXwp\r\n" +
-            "4ImuJ4pi6GfQqlNLdrlax90QxvYXw776ZcWAkomrvSofikbOxwyoXhnxlpM9Bt+H\r\n" +
-            "FBecYGEMNEvCMFdmawIDAQAB\r\n" +
-            "-----END PUBLIC KEY-----";
+    static String tokenUrl = "https://ilesbsanity.insurancearticlez.com/cerberus/connect/token";
 
-    public static String encryptMessage(String plain) {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public ResponseEntity<Map> tokenResponse() {
+        String tokenRequest = null;
+        try {
+            tokenRequest = "grant_type=" + URLEncoder.encode("password", "UTF-8") +
+                    "&username=" + URLEncoder.encode("Spectrum_Ins", "UTF-8") +
+                    "&password=" + URLEncoder.encode("tnKyq5YcDLES6pA", "UTF-8") +
+                    "&scope=" + URLEncoder.encode("esb-kyc", "UTF-8") +
+                    "&client_id=" + URLEncoder.encode("Spectrum_Ins", "UTF-8") +
+                    "&client_secret=" + URLEncoder.encode("T6ksEW6Yd9dHnx8TzSRajc2x", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        HttpHeaders headerToken = new HttpHeaders();
+        headerToken.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<String> requestEntity = new HttpEntity<>(tokenRequest, headerToken);
+        ResponseEntity<Map> tokenResponse = restTemplate.postForEntity(tokenUrl, requestEntity, Map.class);
+
+        if (tokenResponse.getStatusCode() != HttpStatus.OK) {
+            return ResponseEntity.status(tokenResponse.getStatusCode()).build();
+        }
+
+        return ResponseEntity.ok(tokenResponse.getBody());
+    }
+
+    public String encryptMessage(String plain) {
         try {
             Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING");
-            PublicKey publicKey = getPublicKeyFromString(PUBLIC_KEY_STRING);
+            String accessToken = "Bearer " + Objects.requireNonNull(tokenResponse().getBody()).get("access_token");
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", accessToken);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            String payload=accessToken.split("\\.")[1];
+            Gson gson=new Gson();
+            String json=new String(org.apache.commons.codec.binary.Base64.decodeBase64(payload),"UTF-8");
+            JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+            String pbk = jsonObject.get("pbk").getAsString();
+            PublicKey publicKey = getPublicKeyFromString(pbk);
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
             byte[] encrypt = cipher.doFinal(plain.getBytes());
             return Base64.getEncoder().encodeToString(encrypt);
@@ -26,7 +70,7 @@ public class EncryptionUtils {
         }
     }
 
-    private static PublicKey getPublicKeyFromString(String keyString) throws Exception {
+    private PublicKey getPublicKeyFromString(String keyString) throws Exception {
         String publicKeyPEM = keyString
                 .replace("-----BEGIN PUBLIC KEY-----", "")
                 .replaceAll(System.lineSeparator(), "")
@@ -34,11 +78,5 @@ public class EncryptionUtils {
         byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePublic(new X509EncodedKeySpec(encoded));
-    }
-
-    public static void main(String[] args) {
-        String plaintext = "02-10-1869";
-        String encryptedMessage = encryptMessage(plaintext);
-        System.out.println("Encrypted message: " + encryptedMessage);
     }
 }
--- src/main/java/com/example/rsancryption/controller/EncryptRsaController.java
+++ src/main/java/com/example/rsancryption/controller/EncryptRsaController.java
@@ -1,8 +1,11 @@
 package com.example.rsancryption.controller;
 
+import com.example.rsancryption.EncryptionUtils;
 import com.example.rsancryption.service.EncryptAndDecryptService;
 import com.example.rsancryption.service.EncryptRsaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,17 @@ public class EncryptRsaController {
     private EncryptRsaService encryptRsaService;
     @Autowired
     private EncryptAndDecryptService encryptAndDecryptService;
+    @Autowired
+    private EncryptionUtils encryptionUtils;
+    @PostMapping("/msg")
+    public ResponseEntity<String> encryptMsg(@RequestBody String message) {
+        String encryptedMessage = encryptionUtils.encryptMessage(message);
+        if (encryptedMessage != null) {
+            return ResponseEntity.ok(encryptedMessage);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Encryption failed");
+        }
+    }
     @GetMapping("/jwt/decode")
     public String decodeJwt() throws UnsupportedEncodingException {
         return encryptAndDecryptService.decodeJwt();
